70de78c7ed0ec96ed86d7918be8256bc4d4041f9
EIprogramming <61566863+EIprogramming@users.noreply.github.com>
2026-04-04 00:12:13 -0400

    feat: added docstrings to NeuralNet.__init__

--- lib/neural_net.py
+++ lib/neural_net.py
@@ -1,6 +1,3 @@
-import csv
-import time
-
 import numpy as np
 from sklearn.metrics import accuracy_score
 from layer import Layer
@@ -8,6 +5,33 @@ import h5py
 
 class NeuralNet:
     def __init__(self, layer_sizes: tuple, learning_rate: float = 0.01, **kwargs):
+        """Initialize a neural network object.
+
+        Parameters
+        ----------
+
+        layer_sizes :
+            A tuple of the network's layer sizes.
+            First value denotes input size, final value denotes output size. Must have at least an input and output size.
+
+        learning_rate : optional
+            The multiplier applied to the weight change during backpropagation. Default is 0.01.
+
+        loss_method : { "CE", "BCE", "MSE" }, optional
+            The method used to calculate loss. Default is "CE".
+
+        random_state : int or None, optional
+            The random state of the random number generator. Default is None (seed generated from operating system).
+
+        dtype : optional
+            The data type used in the network. Default is np.float64.
+
+            
+        Examples
+        --------
+        >>> NeuralNet((28**2, 256, 10)) # initialize a network with size 784 input layer, 256 hidden layer, and 10 output layer
+        >>> NeuralNet((2, 16, 2), learning_rate = 0.005, loss_method = "MSE", random_state=42, dtype=np.float32)
+        """
         if len(layer_sizes) < 2:
             raise ValueError(f"Shape must have at least two layers")
         
@@ -23,25 +47,57 @@ class NeuralNet:
 
         # learning params
         self.learning_rate = learning_rate
-        self.loss_method = kwargs["loss_method"] if "loss_method" in kwargs else ""
+        self.loss_method = kwargs["loss_method"] if "loss_method" in kwargs else "CE"
         
         self.random_state = kwargs["random_state"] if "random_state" in kwargs else None
         self.rng = np.random.default_rng(self.random_state)
 
-        self.layers: list[Layer] = []
-        prev_layer_size = 0
-        for index, layer_size in enumerate(layer_sizes):
-            if (index == 0):
-                prev_layer_size = layer_size
-                continue # skip the input layer (we already take inputs as they are)
-            self.layers.append(Layer(prev_layer_size, layer_size, dtype=self.dtype, random_state=self.random_state))
-            prev_layer_size = layer_size
+        self.layers: list[Layer] = self.create_layers(layer_sizes)
+
+        # initialize math
+        self.loss = self.CE
+        self.loss_derivative = self.CE_prime
+
+        self.set_loss(self.loss_method)
     
     def __str__(self):
         self_str = ""
         for layer in reversed(self.layers):
             self_str += str(layer) + "\n"
         return self_str
+    
+    def create_layers(self, layer_sizes) -> list[Layer]:
+        layers: list[Layer] = []
+        prev_layer_size = 0
+        for index, layer_size in enumerate(layer_sizes):
+            if (index == 0):
+                prev_layer_size = layer_size
+                continue # skip the input layer (we already take inputs as they are)
+            layers.append(Layer(prev_layer_size, layer_size, dtype=self.dtype, random_state=self.random_state))
+            prev_layer_size = layer_size
+        return layers
+
+    def set_loss(self, loss_method):
+        """Set the method used for the loss function.
+        
+        Parameters
+        ----------
+
+        loss_method: {"CE", "BCE", "MSE"}
+            The method used to calculate the loss.
+        """
+        match loss_method:
+            case "CE":
+                self.loss = self.CE
+                self.loss_derivative = self.CE_prime
+            case "BCE":
+                self.loss = self.BCE
+                self.loss_derivative = self.BCE_prime
+            case "MSE":
+                self.loss = self.MSE
+                self.loss_derivative = self.MSE_prime
+            case _:
+                raise ValueError(f"loss_method must be 'CE', 'BCE', or 'MSE', got {loss_method}")
     
     def process(self, inputs: np.ndarray, mask=False):
         current_inputs = inputs
@@ -68,7 +124,7 @@ class NeuralNet:
 
     def BCE(self, y_pred_all: np.ndarray, y_exp_all: np.ndarray):
         n: int = len(y_pred_all)
-        epsilon = 0.00001
+        epsilon = 1e-8
         loss_BCE = 0
 
         for y_pred, y_exp in zip(y_pred_all, y_exp_all):
@@ -79,8 +135,31 @@ class NeuralNet:
         return loss_BCE
 
     def BCE_prime(self, y_pred, y_exp):
-        epsilon = 0.000001
+        epsilon = 1e-8
         return (y_pred - y_exp) / (y_pred * (1 - y_pred) + epsilon)
+
+    def CE(self, y_pred: np.ndarray, y_exp: np.ndarray):
+        n: int = len(y_pred)
+        epsilon = 1e-8
+
+        loss_CE = - np.sum(y_exp * np.log(y_pred + epsilon), axis=0) / n
+
+        return loss_CE
+
+    # same as BCE prime but repeated to reduce overhead
+    def CE_prime(self, y_pred, y_exp):
+        epsilon = 1e-8
+        return (y_pred - y_exp) / (y_pred * (1 - y_pred) + epsilon)
+    
+    def MSE(self, y_pred: np.ndarray, y_exp: np.ndarray):
+        n: int = len(y_pred)
+
+        loss_MSE = np.sum((y_pred - y_exp)**2, axis=0) / n
+
+        return loss_MSE
+
+    def MSE_prime(self, y_pred, y_exp):
+        return 2 * (y_pred - y_exp)
     
     def grad_softmax_CE(self, y_pred, y_exp):
         return y_pred - y_exp
@@ -150,6 +229,7 @@ class NeuralNet:
         str_accuracy_train = f"TEST ACCURACY: {accuracy_train:.4g}"
 
         accuracy_change = accuracy_train - prev_accuracy_train
+
         if accuracy_change > 0:
             str_accuracy_change = f"TEST ACCURACY CHANGE \033[1;32m{accuracy_change:.4g}\033[0m"
         elif accuracy_change == 0:
@@ -160,16 +240,17 @@ class NeuralNet:
         print(str_epoch, str_loss, str_loss_change, str_accuracy_train, str_accuracy_change, sep="    ")
 
     def train(self, X: np.ndarray, y_exp: np.ndarray, epochs = 10, batch_size=32, validate = None,
-              display=True, verbose=False, lr_scheduling = False, **kwargs):
+              display=True, lr_scheduling = False, **kwargs):
+        # set parameters:
+        
+        noise_normalize = kwargs["noise_normalize"] if "noise_normalize" in kwargs else False
+        dropout = kwargs["dropout"] if "dropout" in kwargs else False
+
         # cast data types:
         X = X.astype(self.dtype)
         y_exp = y_exp.astype(self.dtype)
 
         self.initialize_report(epochs)
-
-        noise_normalize = kwargs["noise_normalize"] if "noise_normalize" in kwargs else False
-        partial_randomization = kwargs["partial_randomization"] if "partial_randomization" in kwargs else False
-        dropout = kwargs["dropout"] if "dropout" in kwargs else False
         
         epoch_losses = np.zeros(epochs)
         prev_accuracy_train = 0
@@ -182,23 +263,11 @@ class NeuralNet:
         lr_k = 0.000000230/batch_size # learning rate decay constant
 
         for epoch in range(epochs):
-            #TOTAL_TIMER_epoch = 0
-            #TOTAL_TIMER_batch = 0
-            #TOTAL_TIMER_backprop = 0
-            #TOTAL_TIMER_backprop1 = 0
-            #TOTAL_TIMER_1 = 0
-            #TIMER_epoch = time.time()
             losses = []
             Y_pred = []
-            shuffled_indices_epoch = shuffled_indices[epoch]#self.rng.permutation(X.shape[0])
+            shuffled_indices_epoch = shuffled_indices[epoch]
             X_shuffled = X[shuffled_indices_epoch]
             y_exp_shuffled = y_exp[shuffled_indices_epoch]
-            # if using partial randomization (i.e. 80% of train data randomly)
-            if partial_randomization:
-                n = 0.25
-                n_percent_of_X = int(n * len(X_shuffled))
-                X_shuffled = X_shuffled[:-n_percent_of_X]
-                y_exp_shuffled = y_exp_shuffled[:-n_percent_of_X]
             
             for batch_number in range(len(X_shuffled)//batch_size):
                 #TIMER_batch = time.time()
@@ -221,10 +290,10 @@ class NeuralNet:
                     # for each layer, starting from the last, go through each node and calculate the deltas
                     raw_output = self.layers[i].batch_raw_outputs
                     if i == len(self.layers) - 1:
-                        if (self.layers[i].activation_method == "softmax"):
+                        if (self.layers[i].activation_method == "softmax" and (self.loss_method == "CE" or self.loss_method == "BCE")):
                             delta_j = self.grad_softmax_CE(y_pred, y_exp_i)
                         else:
-                            delta_j = self.BCE_prime(y_pred, y_exp_i) * self.layers[i].activation_derivative(raw_output)
+                            delta_j = self.loss_derivative(y_pred, y_exp_i) * self.layers[i].activation_derivative(raw_output)
                         self.layers[i].batch_deltas = delta_j
                     else:
                         # sum along the weights and the previous deltas along their respective axes
@@ -264,7 +333,7 @@ class NeuralNet:
                         self.learning_rate = initial_learning_rate * np.exp(- lr_k * adam_t)
 
                     #TOTAL_TIMER_backprop += time.time() - TIMER_backprop
-                loss = self.BCE(y_pred, y_exp_i)
+                loss = self.loss(y_pred, y_exp_i)
                 losses.append(loss)
                 #TOTAL_TIMER_batch += time.time() - TIMER_batch
             
@@ -280,23 +349,20 @@ class NeuralNet:
 
             # display epoch
             if display and epoch % 1 == 0:
-                if verbose: print(f"EPOCH: {epoch + 1}    LOSS: {epoch_loss}    LOSS CHANGE: {epoch_loss - epoch_losses[epoch - 1] if epoch > 0 else 0:.4g}")
+                if validate is not None and len(validate) == 2:
+                    X_test = validate[0]
+                    y_test = validate[1]
+                    y_test_pred = self.predict(X_test)
+                    if ("binary" in kwargs and kwargs["binary"] == True) or ("categorical" in kwargs and kwargs["categorical"] == True):
+                        y_test_pred = np.where(y_test_pred >= 0.5, 1, 0)
+                        if (y_test.shape != y_test_pred.shape and not "binary" in kwargs and kwargs["binary"] == True):
+                            y_test_pred = np.argmax(y_test_pred, axis = -1)
+                    accuracy_train = accuracy_score(y_test_pred, y_test)
+                    self.report["accuracy"][epoch] = accuracy_train
+                    self.print_epoch_report(epoch, epoch_loss, epoch_losses, accuracy_train, prev_accuracy_train)
+                    prev_accuracy_train = accuracy_train
                 else:
-                    if validate is not None and len(validate) == 2:
-                        X_test = validate[0]
-                        y_test = validate[1]
-                        y_test_pred = self.predict(X_test)
-                        if ("binary" in kwargs and kwargs["binary"] == True) or ("categorical" in kwargs and kwargs["categorical"] == True):
-                            y_test_pred = np.where(y_test_pred >= 0.5, 1, 0)
-                            if (y_test.shape != y_test_pred.shape):
-                                y_test_pred = np.argmax(y_test_pred, axis = -1)
-                        accuracy_train = accuracy_score(y_test_pred, y_test)
-                        self.report["accuracy"][epoch] = accuracy_train
-                        self.print_epoch_report(epoch, epoch_loss, epoch_losses, accuracy_train, prev_accuracy_train)
-                        prev_accuracy_train = accuracy_train
-                    else:
-                        
-                        if epoch % 20 == 0: print(f"EPOCH: {epoch + 1}    LOSS: {epoch_loss}    LOSS CHANGE: {epoch_loss - epoch_losses[epoch - 1] if epoch > 0 else 0:.4g}")
+                    if epoch % 20 == 0: print(f"EPOCH: {epoch + 1}    LOSS: {epoch_loss}    LOSS CHANGE: {epoch_loss - epoch_losses[epoch - 1] if epoch > 0 else 0:.4g}")
 
             #TOTAL_TIMER_epoch += time.time() - TIMER_epoch
             #print(f"Timer backprop: {TOTAL_TIMER_backprop:.4g}")
